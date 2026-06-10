e773c59faaaa7b7f54d9f6a15db67cdaf95d9d8d
Udaya Sree <udayasree9955@gmail.com>
2026-06-10 22:52:31 +0530

    Modify instruction memory with new opcodes
    Updated instruction memory initialization with new opcode values.

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -8,14 +8,15 @@ module InstructionMemory(
 reg [31:0] memory [0:255];
 
 initial begin
-    memory[0] = 32'h00430800;
-    memory[1] = 32'h00430800;
-    memory[2] = 32'h00430800;
-    memory[3] = 32'h00430800;
-    memory[4] = 32'h00430800;
-    memory[5] = 32'h00430800;
-    memory[6] = 32'h00430800;
-    memory[7] = 32'h00430800;
+
+    memory[0] = 32'h00430820; // ADD
+    memory[1] = 32'h00430822; // SUB
+    memory[2] = 32'h00430824; // AND
+    memory[3] = 32'h00430825; // OR
+    memory[4] = 32'h00430826; // XOR
+    memory[5] = 32'h0043082A; // SLT
+    memory[6] = 32'h8C010000; // LW
+    memory[7] = 32'hAC010004; // SW
 
 end
 
