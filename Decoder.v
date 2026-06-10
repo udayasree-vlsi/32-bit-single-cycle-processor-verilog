697e6e5ec117c4b2fba00a5fbafe612993518caa
Udaya Sree <udayasree9955@gmail.com>
2026-06-10 22:50:10 +0530

    Update Decoder.v

--- Decoder.v
+++ Decoder.v
@@ -5,7 +5,8 @@ module Decoder(
     output [5:0] opcode,
     output [4:0] rs1,
     output [4:0] rs2,
-    output [4:0] rd
+    output [4:0] rd,
+    output [5:0] funct
 
 );
 
@@ -13,5 +14,6 @@ assign opcode = instruction[31:26];
 assign rs1    = instruction[25:21];
 assign rs2    = instruction[20:16];
 assign rd     = instruction[15:11];
+assign funct  = instruction[5:0];
 
 endmodule
