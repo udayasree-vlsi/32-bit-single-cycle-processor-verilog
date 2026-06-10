75444fac924a3b8e0e99e50e4552ccf8a3e3cf51
Udaya Sree <udayasree9955@gmail.com>
2026-06-10 22:54:33 +0530

    Clean up comments in PC.v
    Removed header comments from PC.v file.

--- PC.v
+++ PC.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.05.2026 23:30:31
-// Design Name: 
-// Module Name: PC
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 module PC (
     input clk,
